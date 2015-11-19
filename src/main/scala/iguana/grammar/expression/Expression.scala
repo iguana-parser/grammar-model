a10dbb4efa54127bb633f35c7fd5066cf484334e
Anastassija <A.Izmaylova@cwi.nl>
2015-11-19 16:05:06 +0100

    Re-designing the symbol hierarchy.

--- src/main/scala/iguana/grammar/expression/Expression.scala
+++ src/main/scala/iguana/grammar/expression/Expression.scala
@@ -50,12 +50,36 @@ trait ExpressionType extends AbstractASTType {
     }
   }
   
+  case class Name(val name: java.lang.String, val i: java.lang.Integer) extends Expression {
+    def accept[T](v: ASTVisitor[T]) = v.visitName(this)
+    override def toString = if (i == -1) name else (name + ":" + i)
+  }
+  
+  object Name { def apply(name: java.lang.String): Name = Name(name, -1) }
+  
+  abstract class Call extends Expression {
+    def fun: java.lang.String
+    def arguments: java.util.List[Expression]
+    def accept[T](v: ASTVisitor[T]) = v.visitCall(this)
+    override def toString = {
+      val args = if (arguments.isEmpty())
+                   ""
+                 else {
+                   val as: Iterable[Expression] = arguments
+                   as.tail.foldLeft(as.head.toString())((s, e) => s + "," + e.toString())
+                 }
+      fun + "(" + args + ")" 
+    }
+  }
+  
   trait AbstractASTVisitor[T] extends super.AbstractASTVisitor[T] {
     def visitBoolean(expression: Boolean): T
     def visitInteger(expression: Integer): T
     def visitReal(expression: Real): T
     def visitString(expression: String): T
     def visitTuple(expression: Tuple): T
+    def visitName(expression: Name): T
+    def visitCall(expression: Call): T
   }
   
 }

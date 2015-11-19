371dbcf1a98cb0e16259b0bd86fb01a692785d09
Anastassija <A.Izmaylova@cwi.nl>
2015-11-19 17:39:11 +0100

    Re-designing the symbol hierarchy.

--- src/main/scala/iguana/grammar/expression/Statement.scala
+++ src/main/scala/iguana/grammar/expression/Statement.scala
@@ -1,16 +1,53 @@
+/*
+ * Copyright (c) 2015, Ali Afroozeh and Anastasia Izmaylova, Centrum Wiskunde & Informatica (CWI)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this 
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this 
+ *    list of conditions and the following disclaimer in the documentation and/or 
+ *    other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+ * OF SUCH DAMAGE.
+ *
+ */
 package iguana.grammar.expression
 
 /**
  * @author Anastasia Izmaylova
  */
-trait StatementType extends AbstractASTType {
+trait StatementType extends AbstractASTType with ExpressionType with VariableDeclarationType {
   
   type ASTVisitor[T] <: AbstractASTVisitor[T]
   
   trait Statement extends AbstractAST
   
+  case class ExpressionStatement(val expression: Expression) extends Statement {
+    def accept[T](v: ASTVisitor[T]) = v.visitExpressionStatement(this)
+    override def toString = expression.toString()
+  }
+  
+  case class VariableDeclarationStatement(val declaration: VariableDeclaration) extends Statement {
+    def accept[T](v: ASTVisitor[T]) = v.visitVariableDeclarationStatement(this)
+    override def toString = declaration.toString()
+  }
+  
   trait AbstractASTVisitor[T] extends super.AbstractASTVisitor[T] {
-    
+    def visitExpressionStatement(statement: ExpressionStatement): T
+    def visitVariableDeclarationStatement(statement: VariableDeclarationStatement): T
   }
   
 }
